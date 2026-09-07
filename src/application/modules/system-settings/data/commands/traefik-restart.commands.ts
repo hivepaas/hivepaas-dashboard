a8bed8796187c42c9e9286784e1e341928185cd4
tiendc <tiendc@gmail.com>
2026-09-07 14:41:03 +0700

    Fix UI restart traefik service

--- src/application/modules/system-settings/data/commands/traefik-restart.commands.ts
+++ src/application/modules/system-settings/data/commands/traefik-restart.commands.ts
@@ -1,16 +1,15 @@
 import { type UseMutationOptions, useMutation } from "@tanstack/react-query";
 import { useTraefikRestartApi } from "~/system-settings/api/hooks";
-import type { TraefikRestart_Execute_Req, TraefikRestart_Execute_Res } from "~/system-settings/api/services";
+import type { TraefikRestart_Execute_Res } from "~/system-settings/api/services";
 
-type RestartReq = TraefikRestart_Execute_Req["data"] | undefined;
 type RestartRes = TraefikRestart_Execute_Res;
-type RestartOptions = Omit<UseMutationOptions<RestartRes, Error, RestartReq>, "mutationFn">;
+type RestartOptions = Omit<UseMutationOptions<RestartRes>, "mutationFn">;
 
 function useRestart({ onSuccess, ...options }: RestartOptions = {}) {
     const { mutations } = useTraefikRestartApi();
 
     return useMutation({
-        mutationFn: (data?: RestartReq) => mutations.execute(data ?? {}),
+        mutationFn: () => mutations.execute({}),
         onSuccess: (response, ...rest) => {
             onSuccess?.(response, ...rest);
         },
